d7072b3fc88c0811c553a02db2ac2406444d2265
alfy321 <alfigyf@gmail.com>
2023-03-15 10:48:49 +0800

    api test

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,18 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HolidaysController;
-<<<<<<< Updated upstream
-=======
-use App\Http\Controllers\OfficeController;
-use App\Http\Controllers\PersonalDataSheetController;
->>>>>>> Stashed changes
-use App\Http\Controllers\SalaryGradeController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HolidaysController;
+
+use App\Http\Controllers\SalaryGradeController;
+use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\PositionController;
+use App\Http\Controllers\QualificationStandardController;
+use App\Http\Controllers\PersonalDataSheetController;
+use App\Http\Controllers\PositionDescriptionController;
+
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -21,29 +23,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::resource('/holidays', HolidaysController::class);
 
+
     Route::resource('/salary-grade', SalaryGradeController::class);
-
-<<<<<<< Updated upstream
-=======
     Route::resource('/position', PositionController::class);
-
+    Route::resource('/office', OfficeController::class);
     Route::resource('/position-description', PositionDescriptionController::class);
-
     Route::resource('/qualification-standard', QualificationStandardController::class);
-
     Route::resource('/personal-data-sheet', PersonalDataSheetController::class);
 
-   
 
 
-
-   
-
->>>>>>> Stashed changes
+    
 });
