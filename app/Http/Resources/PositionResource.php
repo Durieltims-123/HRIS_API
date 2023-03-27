2baacd8346beada5bcdfa90e21cf52abbc3b1dc6
Quennan Serbias T. Tunguia <tunguia.quennanserbias12@gmail.com>
2023-03-27 15:12:35 +0800

    vacancy resources

--- app/Http/Resources/PositionResource.php
+++ app/Http/Resources/PositionResource.php
@@ -18,24 +18,29 @@ class PositionResource extends JsonResource
             "id" => (string)$this->id,
             "attributes" => [
                 //salary grade
-                "number" => (string)$this->belongsToSalaryGrade->number,
-                "amount" => (string)$this->belongsToSalaryGrade->amount,
-
-                //position
+                // "number" => (string)$this->belongsToSalaryGrade->number,
+                // "amount" => (string)$this->belongsToSalaryGrade->amount,
                 "title" => (string)$this->title,
                 "salary_grade_id" => (string)$this->salary_grade_id,
 
+                'salary_grade' => new SalaryGradeResource($this->whenLoaded('belongsToSalaryGrade')),
+
+                //position
+                
+
+                'qualification_standards' => QualificationStandardResource::collection($this->whenLoaded('hasManyQualificationStandard') ),
+
                 //qualification standards
-                "position_id" => (string)$this->hasManyQualificationStandard[0]->position_id,
-                "education" => (string)$this->hasManyQualificationStandard[0]->education,
-                "training" => (string)$this->hasManyQualificationStandard[0]->training,
-                "experience" => (string)$this->hasManyQualificationStandard[0]->experience,
-                "eligibility" => (string)$this->hasManyQualificationStandard[0]->eligibility,
-                "competency" => (string)$this->hasManyQualificationStandard[0]->competency,
+                // "position_id" => (string)$this->hasManyQualificationStandard[0]->position_id,
+                // "education" => (string)$this->hasManyQualificationStandard[0]->education,
+                // "training" => (string)$this->hasManyQualificationStandard[0]->training,
+                // "experience" => (string)$this->hasManyQualificationStandard[0]->experience,
+                // "eligibility" => (string)$this->hasManyQualificationStandard[0]->eligibility,
+                // "competency" => (string)$this->hasManyQualificationStandard[0]->competency,
             ],
             // "plantilla" => new VacancyResource($this->whenLoaded('hasManyPlantilla')),
-            'salary_grade' => new SalaryGradeResource($this->whenLoaded('belongsToSalaryGrade')),
-            'qualification_standards' => QualificationStandardResource::collection($this->whenLoaded('hasManyQualificationStandard') ),
+            
+            
 
         ];
     }
