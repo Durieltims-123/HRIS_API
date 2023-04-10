7eff6a418143846eec78e031a6a2a3a20d0324ac
PenguSoup <99572768+PenguSoup@users.noreply.github.com>
2023-04-10 21:54:25 +0800

    Tailwind Installed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+
+    // Add your template path files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
